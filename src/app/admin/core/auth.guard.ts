017409d208d55adc4cdc93f8595b2ee749bc46d5
Yummic dev <development@yummic.com>
2019-05-24 13:11:24 +0530

    updated dependencies

--- src/app/admin/core/auth.guard.ts
+++ src/app/admin/core/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
+import { map, take, tap} from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { AngularFireAuth } from 'angularfire2/auth';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
